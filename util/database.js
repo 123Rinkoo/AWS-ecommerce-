af01073a724182f396a50b2f0b39d85c0b0898b2
rinkoo <rinkookandpal1966@gmail.com>
2022-11-29 11:24:20 +0530

    initial commit

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(`${process.env.TableName}`, 'root', `${process.env.Password}` , {
+const sequelize = new Sequelize(`${process.env.TableName}`, `${process.env.User_Name}`, `${process.env.Password}` , {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.Host
 });
 
 module.exports = sequelize;
